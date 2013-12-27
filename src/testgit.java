521aafd0d4b43dc7fab4af296918ef8af426ca03
xuezy90 <xuezy90@gmail.com>
2013-12-27 14:07:55 +0800

    dd

--- src/testgit.java
+++ src/testgit.java
@@ -7,6 +7,7 @@ public class testgit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello git");
+		System.out.println("Hello git11");
 	}
 
 }
