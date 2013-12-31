b099343ef1f804427ba19383a0d58d87de8298fe
xue <xuezy90@126.com>
2013-12-31 14:49:24 +0800

    add

---
+++ src/TestCalc.java
@@ -0,0 +1,12 @@
+
+public class TestCalc {
+	public static double Add(double a,double b)
+	{
+		return a+b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
