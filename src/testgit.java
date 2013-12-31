c03c6655ff843e7a34f088a96ead7e75d3ad3d1e
xue <xuezy90@126.com>
2013-12-31 14:46:34 +0800

    test

--- src/testgit.java
+++ src/testgit.java
@@ -7,7 +7,7 @@ public class testgit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello git");
-		System.out.println("Hello git11");
+		System.out.println("Hello Allen Xue");
 	}
 
 }
